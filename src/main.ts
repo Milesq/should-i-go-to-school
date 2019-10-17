a1efc92d7dd83e0f1f080f20078c966502e40932
Milesq <wmilosz88@gmail.com>
2019-10-17 22:36:46 +0200

    Add Commanderjs

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,25 @@ require('dotenv').config();
 
 import downloadData from './downloadData';
 
-downloadData().catch(error => {
-    console.log(error);
-    process.exit();
-});
+const program = require('commander');
+
+function download() {
+    downloadData().catch(error => {
+        console.log(error);
+        process.exit();
+    });
+}
+
+function logLesson(name: string) {
+    console.log('Lesson: ', name);
+}
+
+function nextLesson() {
+    logLesson('Matematyka');
+}
+
+program.option('-d, --download-data', '', download)
+    .option('-n, --next-lesson', '', nextLesson)
+    .option('-l, --lesson [name]', 'Lesson name', logLesson);
+
+program.parse(process.argv);
