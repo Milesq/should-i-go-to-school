8d4c85bf21f606736c3441c49b61e6a2586feece
Milesq <wmilosz88@gmail.com>
2019-10-19 00:32:36 +0200

    Add --summary switch"

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
+import { readFileSync } from 'fs';
 import downloadData from './downloadData';
+import Lesson from './Lesson';
 
 const program = require('commander');
 
@@ -19,9 +21,18 @@ function nextLesson() {
     logLesson('Matematyka');
 }
 
+function showSummary() {
+    const frequencyData = JSON.parse(readFileSync('./data.json', 'utf-8'));
+    frequencyData.forEach((el: Lesson) => {
+        const percentage = (el.presence / (el.absence + el.presence)) * 100;
+        console.log(Math.round(percentage * 100) / 100 + '%\t\t|\t\t', el.type);
+    });
+}
+
 program
     .option('-d, --download-data', '', download)
     .option('-n, --next-lesson', '', nextLesson)
+    .option('-s, --summary', '', showSummary)
     .option('-l, --lesson [name]', 'Lesson name', logLesson);
 
 program.parse(process.argv);
